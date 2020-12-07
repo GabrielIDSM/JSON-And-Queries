bf32f6c4665be1e2ce7cb162670fe629a3f6187b
GabrielInacio <gabrielinacio@id.uff.br>
2020-12-07 13:16:33 -0300

    New Example

--- Language Projects/json-query Project/main.js
+++ Language Projects/json-query Project/main.js
@@ -3,3 +3,7 @@ import {newJSON} from './newJSON.js'
 newJSON('Request', 'reports', (newValue) => {
     console.log(newValue)
 })
+
+newJSON('Request', 'reports[params=0].maintainability', (newValue) => {
+    console.log(newValue)
+})
