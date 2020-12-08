bf2cc3c838ca8d1be020a42862f58afe8c73dd90
GabrielInacio <gabrielinacio@id.uff.br>
2020-12-08 09:28:07 -0300

    New console lines

--- Language Projects/json-query Project/main.js
+++ Language Projects/json-query Project/main.js
@@ -1,9 +1,14 @@
 import {newJSON} from './newJSON.js'
 
+console.log("\n\n==========JSON-QUERY PROJECT==========\n")
 newJSON('Request', 'reports', (newValue) => {
+    console.log("\nQUERY 1: reports\nRESULT:\n")
     console.log(newValue)
+    console.log("\nTYPEOF: " + typeof newValue)
 })
 
 newJSON('Request', 'reports[params=0].maintainability', (newValue) => {
+    console.log("\nQUERY 2: reports[params=0].maintainability\nRESULT:\n")
     console.log(newValue)
+    console.log("\nTYPEOF: " + typeof newValue)
 })
