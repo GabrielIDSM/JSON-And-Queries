b19dad1c6275dccb6e65fd8a4f3ab5de1861760a
GabrielInacio <gabrielinacio@id.uff.br>
2020-12-09 12:56:53 -0300

    Folder renamed

--- Projects/json-query Project/main.js
+++ Projects/json-query Project/main.js
@@ -2,13 +2,13 @@ import {newJSON} from './newJSON.js'
 
 console.log("\n\n==========JSON-QUERY PROJECT==========\n")
 newJSON('Request', 'reports', (newValue) => {
-    console.log("\nQUERY 1: reports\nRESULT:\n")
+    console.log("\nQUERY 1 (BASIC): reports\nRESULT:\n")
     console.log(newValue)
     console.log("\nTYPEOF: " + typeof newValue)
 })
 
 newJSON('Request', 'reports[params=0].maintainability', (newValue) => {
-    console.log("\nQUERY 2: reports[params=0].maintainability\nRESULT:\n")
+    console.log("\nQUERY 2 (WHERE): reports[params=0].maintainability\nRESULT:\n")
     console.log(newValue)
     console.log("\nTYPEOF: " + typeof newValue)
 })
