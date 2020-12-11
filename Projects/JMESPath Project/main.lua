b456b09bc38766a52be02bf2d6de72917ed89560
GabrielIDSM <gabrielinacio@id.uff.br>
2020-12-11 12:03:57 -0300

    First Query

--- Projects/JMESPath Project/main.lua
+++ Projects/JMESPath Project/main.lua
@@ -1,6 +1,17 @@
+--IMPORT
 local jmespath = require "jmespath"
-local expression = "foo.baz"
-local data = { foo = { baz = "bar" } }
-local result = jmespath.search(expression, data)
+local jsonStorage = require("jsonStorage")
 
-print(result)
+--TESTE
+local expression = "foo.baz"
+local data = { foo = { baz = "bar" } } --JMESPath utiliza tables
+local result = jmespath.search(expression, data)
+print("TESTE: \n" .. result)
+
+--Request.json
+data = jsonStorage.loadTable("Request.json")
+
+--CONSULTA SIMPLES
+expression = "reports[1].params"
+result = jmespath.search(expression, data)
+print("\nCONSULTA SIMPLES: \n" .. result)
