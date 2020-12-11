6b8e1300f6a09672f6ecc35e7e5de0e9c76627b4
GabrielIDSM <gabrielinacio@id.uff.br>
2020-12-11 13:05:25 -0300

    WHERE Query

--- Projects/JMESPath Project/main.lua
+++ Projects/JMESPath Project/main.lua
@@ -15,3 +15,15 @@ data = jsonStorage.loadTable("Request.json")
 expression = "reports[1].params"
 result = jmespath.search(expression, data)
 print("\nCONSULTA SIMPLES: \n" .. result)
+
+--WHERE
+expression = "reports[?path==`cli.js`]"
+result = jmespath.search(expression, data)
+print("\nWHERE: \n")
+for index, data in ipairs(result) do
+    print(index)
+
+    for key, value in pairs(data) do
+        print('\t', key, value)
+    end
+end
