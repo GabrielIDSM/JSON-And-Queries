3c4e91167729ac0e607ebe07457e57055fbe1b30
GabrielInacio <gabrielinacio@id.uff.br>
2020-12-18 12:43:11 -0300

    jsonq - query 1

--- Projects/jsonq Project/main.php
+++ Projects/jsonq Project/main.php
@@ -1,2 +1,13 @@
-use Nahid/JsonQ/Jsonq;
+<?php
+
+require_once 'vendor/autoload.php';
+
+use Nahid\JsonQ\Jsonq;
+
 $jsonq = new Jsonq('Request.json');
+
+$res = $jsonq->from('reports')
+    ->get();
+echo($res);
+
+?>
